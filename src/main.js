49075af4a472d2ea1caca80cd4f551714baae6d5
BaeSangUng <dentist312@naver.com>
2020-11-20 10:26:59 +0900

    npm install vue bootstrap-vue bootstrap

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VModal from 'vue-js-modal';
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
 Vue.use(VModal, { dynamic: true });
 
 new Vue({
