c65bd5d9d12694b46bf06957f1e069310bab037e
BaeSangUng <bsw0312009@gmail.com>
2020-11-24 01:49:47 +0900

    Jjim vue

---
+++ src/components/Search/AptDetail.vue
@@ -0,0 +1,83 @@
+<template>
+  <div v-if="apt">
+    <h3>{{ apt.아파트 }}</h3>
+    <img src="@/assets/건물.jpg" alt="행복 아파트" />
+    <div style="margin: 10px"></div>
+    <ul align="left">
+      <li>일련번호 : {{ apt.일련번호 }}</li>
+      <li>아파트 이름 : {{ apt.아파트 }}</li>
+      <li>법정동 : {{ apt.법정동 }}</li>
+      <li>층수 : {{ apt.층 }}층</li>
+      <li>
+        거래금액 : {{ (apt.거래금액.replace(',', '') * 10000) | price }}원
+      </li>
+    </ul>
+
+    <button v-model="heart" @click="jjim">{{ heart }}</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { mapGetters } from 'vuex';
+export default {
+  name: 'AptDetail',
+  props: {
+    apt: [Object, String],
+  },
+  component: {
+    heart: '♡',
+  },
+  computed: {
+    ...mapGetters(['getAccessToken', 'getUserId', 'getUserPw', 'getUserName']),
+  },
+  filters: {
+    price: function(value) {
+      if (!value) return value;
+      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    },
+  },
+  created() {
+    if (
+      sessionStorage.getItem('accessToken') != null &&
+      sessionStorage.getItem('accessToken') != ''
+    ) {
+      this.$store.state.accessToken = sessionStorage.getItem('accessToken');
+      this.$store.state.userId = sessionStorage.getItem('userId');
+      this.$store.state.userPw = sessionStorage.getItem('userPw');
+      this.$store.state.userName = sessionStorage.getItem('userName');
+    }
+    var temp = {
+      num: this.apt.일련번호,
+      userId: this.$store.state.userId,
+    };
+    axios.get('url', temp).then((response) => {
+      if (response.data) {
+        this.heart = '♥';
+      } else {
+        this.heart = '♡';
+      }
+    });
+  },
+  methods: {
+    jjim() {
+        var temp = {
+          num: this.apt.일련번호,
+          userId: this.$store.state.userId,
+        };
+      if (this.heart == '♥') {
+        axios.delete('url', temp).then(()=>{
+            this.heart = '♡';
+        }).catch(()=>alert("delete error"););
+      }
+      else{
+        axios.post('url', temp).then(()=>{
+            this.heart = '♥';
+        }).catch(()=>alert("post error"););
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -4,16 +4,31 @@
     <div class="row">
       <SearchBar @send-dong-code="sendDongCode" />
       <div>
-        가격 : <input type="range" id="pricerange" min="0" max="3000000" step="10" v-model="limit_price">
-              <input type="text" v-model="limit_price"> 이하<br>
-        면적 : <input type="range" id="widthrange" min="0" max="200" step="10" v-model="limit_width">
-              <input type="text" v-model="limit_width"> 이하<br>
+        가격 :
+        <input
+          type="range"
+          id="pricerange"
+          min="0"
+          max="3000000"
+          step="10"
+          v-model="limit_price"
+        />
+        <input type="text" v-model="limit_price" /> 이하<br />
+        면적 :
+        <input
+          type="range"
+          id="widthrange"
+          min="0"
+          max="200"
+          step="10"
+          v-model="limit_width"
+        />
+        <input type="text" v-model="limit_width" /> 이하<br />
       </div>
 
       <input
         type="text"
         v-if="sendDongCode != null"
-        v-model="apt"
         v-on:keyup.enter="searchByAptName"
       />
       <button @click="searchByAptName">검색</button>
@@ -23,7 +38,7 @@
         <ul
           v-for="(item, index) in showaptsList"
           :key="index"
-          @select-apt="selectApt"
+          @click="selectApt(item)"
         >
           <li><img src="@/assets/건물.jpg" alt="건물사진" /></li>
           <li>
@@ -45,8 +60,20 @@
       </div>
       <div class="col">
         <div>
-          <gmap-map :center="center" :zoom="12" style="width=100%;  height:600px;" >
-           <gmap-marker :key="index" v-for="(m, index) in markers" :position="m.position" @click="markerClicked(m)" ></gmap-marker>
+          <apt-detail :apt="selectedApt" />
+        </div>
+        <div>
+          <gmap-map
+            :center="center"
+            :zoom="12"
+            style="width=100%;  height:600px;"
+          >
+            <gmap-marker
+              :key="index"
+              v-for="(m, index) in markers"
+              :position="m.position"
+              @click="markerClicked(m)"
+            ></gmap-marker>
           </gmap-map>
         </div>
       </div>
@@ -57,12 +84,13 @@
 <script>
 import axios from 'axios';
 import SearchBar from '@/components/Search/searchBar.vue';
-// import AptDetail from '@/components/AptDetail.vue';
+import AptDetail from '@/components/Search/AptDetail.vue';
 
 export default {
   name: 'Apt',
   components: {
     SearchBar,
+    AptDetail,
   },
   data() {
     return {
@@ -73,26 +101,25 @@ export default {
       aptsSize: '',
       showaptsList: [],
       apt: '',
-      position:[],
+      position: [],
 
-      limit_price:'3000000',
-      limit_width:'200',
-
+      limit_price: '3000000',
+      limit_width: '200',
 
       center: { lat: 37.5665734, lng: 126.978179 },
-      zoom: "18",
+      zoom: '18',
       markers: [],
       places: [],
-      currentPlace: null
+      currentPlace: null,
     };
   },
   mounted() {
     this.geolocate();
   },
-  computed : {
-    changedMarkers :function() {
+  computed: {
+    changedMarkers: function() {
       return this.markers;
-    }
+    },
   },
   methods: {
     sendDongCode: function(dongCode) {
@@ -122,39 +149,51 @@ export default {
       // console.log(this.apts);
       this.showaptsList = [];
       console.log(this.apts);
-        for (var i = 0; i < this.aptsSize; i++) {
-          if (this.apts[i].아파트.search(this.apt) != -1){
-            if(this.apts[i].법정동읍면동코드 == this.dongCode.substring(5, 10)){
-              console.log(parseInt(this.apts[i].거래금액.replace(',','')));
-              if(parseInt(this.apts[i].거래금액.replace(',','')) <= this.limit_price && this.apts[i].전용면적 <=this.limit_width){
-                console.log(this.apts[i]);
-                this.showaptsList.push(this.apts[i]);
-              }
-            } 
+      for (var i = 0; i < this.aptsSize; i++) {
+        if (this.apts[i].아파트.search(this.apt) != -1) {
+          if (this.apts[i].법정동읍면동코드 == this.dongCode.substring(5, 10)) {
+            console.log(parseInt(this.apts[i].거래금액.replace(',', '')));
+            if (
+              parseInt(this.apts[i].거래금액.replace(',', '')) <=
+                this.limit_price &&
+              this.apts[i].전용면적 <= this.limit_width
+            ) {
+              console.log(this.apts[i]);
+              this.showaptsList.push(this.apts[i]);
+            }
           }
         }
-
-      this.position = [];
-      for(var j = 0; j < this.showaptsList.length; j++){
-        var val1 = this.showaptsList[j].도로명;
-        var val2 = this.showaptsList[j].도로명건물본번호코드;
-        val2 = val2.replace(/(^0+)/, "");
-
-        axios
-        .get("https://maps.googleapis.com/maps/api/geocode/json?address=" + val1 + "+" + val2 + "&key=AIzaSyCmBgay0wb9KagZHoyRCc5lbQ_PPdywdfE")
-        .then((response)=>{
-          var temp = {lat : response.data.results[0].geometry.location.lat, lng : response.data.results[0].geometry.location.lng};
-          this.position.push(temp);
-          this.addMarker(temp);
-          console.log(response.data);
-        })
-        .catch((ex)=>{
-          console.log("error : " + ex);
-        });
       }
 
-    console.log(this.markers);
+      this.position = [];
+      for (var j = 0; j < this.showaptsList.length; j++) {
+        var val1 = this.showaptsList[j].도로명;
+        var val2 = this.showaptsList[j].도로명건물본번호코드;
+        val2 = val2.replace(/(^0+)/, '');
 
+        axios
+          .get(
+            'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+              val1 +
+              '+' +
+              val2 +
+              '&key=AIzaSyCmBgay0wb9KagZHoyRCc5lbQ_PPdywdfE'
+          )
+          .then((response) => {
+            var temp = {
+              lat: response.data.results[0].geometry.location.lat,
+              lng: response.data.results[0].geometry.location.lng,
+            };
+            this.position.push(temp);
+            this.addMarker(temp);
+            console.log(response.data);
+          })
+          .catch((ex) => {
+            console.log('error : ' + ex);
+          });
+      }
+
+      console.log(this.markers);
     },
     selectApt: function(apt) {
       this.selectedApt = apt;
@@ -165,31 +204,30 @@ export default {
     },
     addMarker(position) {
       if (position) {
-        this.markers.push({position : position});
+        this.markers.push({ position: position });
         this.places.push(this.currentPlace);
         this.center = position;
         this.currentPlace = null;
       }
     },
     geolocate() {
-      navigator.geolocation.getCurrentPosition(position => {
+      navigator.geolocation.getCurrentPosition((position) => {
         this.center = {
           lat: position.coords.latitude,
-          lng: position.coords.longritude
+          lng: position.coords.longritude,
         };
         this.markers.push({
-          position: { 
+          position: {
             lat: position.coords.latitude,
-            lng: position.coords.longitude
-         },
+            lng: position.coords.longitude,
+          },
         });
       });
-      
     },
-    markerClicked(m){
-      this.center=m.position;
+    markerClicked(m) {
+      this.center = m.position;
       console.log(m.position);
-    }
+    },
   },
 };
 </script>
