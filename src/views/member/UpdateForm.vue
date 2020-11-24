c201e554bd130b38061a7f6f0a39ac94fc962c26
고재석 <gojeasuk2@naver.com>
2020-11-24 20:37:52 +0900

    dfsajp

--- src/views/member/UpdateForm.vue
+++ src/views/member/UpdateForm.vue
@@ -2,22 +2,29 @@
   <div>
     <table>
       <tr>
-        <td>ID :</td>
+        <td colspan="2">
+        <h3 class="modal-title" style="text-align:right; width:100%;">개인정보 수정</h3>
+        </td>
+      </tr>
+      <tr>
+        <td style="text-align:right; width:40%;">ID :</td>
         <td><input type="text" disabled v-model="user.userid"/></td>
       </tr>
       <tr>
-        <td>PW :</td>
+        <td style="text-align:right;">PW :</td>
         <td><input type="text" v-model="user.userpw"/></td>
       </tr>
       <tr>
-        <td>NAME :</td>
+        <td style="text-align:right;">NAME :</td>
         <td><input type="text" v-model="user.username"/></td>
       </tr>
+      <tr><td>&nbsp;</td></tr>
+      <tr><td>&nbsp;</td></tr>
       <tr>
-        <td colspan="2">
-          <button @click="update">수정</button>
-          <button @click="del">삭제</button>
-          <button @click="$emit('close')">취소</button>
+        <td colspan="2" style="text-align:center;">
+          <button @click="update" class="btn btn-primary">수정</button>
+          <button @click="del" class="btn btn-danger">삭제</button>
+          <button @click="$emit('close')" class="btn btn-secondary">취소</button>
         </td>
       </tr>
     </table>
